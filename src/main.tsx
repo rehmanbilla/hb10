d54b867c2041bed4b5304e83bd764f5a134dd52e
Noul99 <ashrafmemorialhighschool@gmail.com>
2025-05-10 00:37:40 +0500

    Fix routing by adding basename to BrowserRouter

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import PrivacyPolicy from './PrivacyPolicy.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/hb10">
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/privacy-policy" element={<PrivacyPolicy />} />
